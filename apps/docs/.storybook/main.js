dd70036d6be7a368572839bdee0f19fe2dcd1973
clianor <ejrgus94160@gmail.com>
2023-02-05 02:26:18 +0900

    Fix:스토리북 builder-vite -> webpack으로 변경
    Signed-off-by: clianor <ejrgus94160@gmail.com>

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -1,14 +1,13 @@
-import { StorybookConfig } from "@storybook/react-vite";
-
-const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.(js|jsx|ts|tsx)"],
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: "@storybook/react-webpack5",
     options: {},
   },
   docs: {
