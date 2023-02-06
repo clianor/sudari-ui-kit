206b3366aaee6de0e819258b5196f689d28fc4a2
clianor <ejrgus94160@gmail.com>
2023-02-07 01:37:01 +0900

    Fix: change bundler for ui
    tsup -> rollup

---
+++ packages/ui/rollup.config.mjs
@@ -0,0 +1,38 @@
+import { babel } from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import json from "@rollup/plugin-json";
+import { terser } from "rollup-plugin-terser";
+
+const inputSrc = [
+  ["./src/index.tsx", "es"],
+  ["./src/index.tsx", "cjs"],
+];
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
+
+export default inputSrc.map(([input, format]) => {
+  return {
+    input,
+    output: {
+      dir: `dist/${format}`,
+      format,
+      exports: "auto",
+      preserveModules: format === "cjs",
+    },
+    external: [/@babel\/runtime/],
+    plugins: [
+      babel({
+        extensions,
+        babelHelpers: "runtime",
+        include: ["./src/**/*"],
+        exclude: "node_modules/**",
+      }),
+      nodeResolve({ extensions }),
+      commonjs({ include: "node_modules/**" }),
+      peerDepsExternal(),
+      json(),
+      terser(),
+    ],
+  };
+});
