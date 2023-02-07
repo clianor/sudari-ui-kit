483c4f34c6ce7f1428f15b81d0e463f25d076a5d
clianor <ejrgus94160@gmail.com>
2023-02-07 21:11:20 +0900

    Feat: emotion 셋팅

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -1,9 +1,18 @@
 import * as React from "react";
+import { css } from "@emotion/react";
 
 export interface ButtonProps {
   label?: string;
 }
 
 export const Button: React.FC<ButtonProps> = ({ label }) => {
-  return <button>{label}</button>;
+  return (
+    <button
+      css={css`
+        color: red;
+      `}
+    >
+      {label}
+    </button>
+  );
 };
