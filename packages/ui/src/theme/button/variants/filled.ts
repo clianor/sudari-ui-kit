a2bb9f106177048865437e71ef387769b7078ad7
clianor <ejrgus94160@gmail.com>
2023-02-12 01:36:30 +0900

    Refac: ui 패키지 비대해진 button의 variants 파일 분리

---
+++ packages/ui/src/theme/button/variants/filled.ts
@@ -0,0 +1,53 @@
+import tw from 'twin.macro';
+
+export default {
+  white: [
+    tw`bg-white text-slate-900`,
+    tw`shadow-md shadow-slate-500/20`,
+    tw`hover:(shadow-lg shadow-slate-500/40)`,
+    tw`focus:(opacity-80 shadow-none)`,
+    tw`active:(opacity-80 shadow-none)`,
+  ],
+  gray: [
+    tw`text-white bg-gray-500`,
+    tw`shadow-md shadow-gray-500/20`,
+    tw`hover:(shadow-lg shadow-gray-500/40)`,
+    tw`focus:(opacity-80 shadow-none)`,
+    tw`active:(opacity-80 shadow-none)`,
+  ],
+  yellow: [
+    tw`text-white bg-yellow-500`,
+    tw`shadow-md shadow-yellow-500/20`,
+    tw`hover:(shadow-lg shadow-yellow-500/40)`,
+    tw`focus:(opacity-80 shadow-none)`,
+    tw`active:(opacity-80 shadow-none)`,
+  ],
+  green: [
+    tw`text-white bg-green-500`,
+    tw`shadow-md shadow-green-500/20`,
+    tw`hover:(shadow-lg shadow-green-500/40)`,
+    tw`focus:(opacity-80 shadow-none)`,
+    tw`active:(opacity-80 shadow-none)`,
+  ],
+  blue: [
+    tw`text-white bg-blue-500`,
+    tw`shadow-md shadow-blue-500/20`,
+    tw`hover:(shadow-lg shadow-blue-500/40)`,
+    tw`focus:(opacity-80 shadow-none)`,
+    tw`active:(opacity-80 shadow-none)`,
+  ],
+  red: [
+    tw`text-white bg-red-500`,
+    tw`shadow-md shadow-red-500/20`,
+    tw`hover:(shadow-lg shadow-red-500/40)`,
+    tw`focus:(opacity-80 shadow-none)`,
+    tw`active:(opacity-80 shadow-none)`,
+  ],
+  purple: [
+    tw`text-white bg-purple-500`,
+    tw`shadow-md shadow-purple-500/20`,
+    tw`hover:(shadow-lg shadow-purple-500/40)`,
+    tw`focus:(opacity-80 shadow-none)`,
+    tw`active:(opacity-80 shadow-none)`,
+  ],
+};
---
+++ packages/ui/src/theme/button/variants/outlined.ts
@@ -0,0 +1,47 @@
+import tw from 'twin.macro';
+
+export default {
+  white: [
+    tw`border border-white`,
+    tw`text-white`,
+    tw`hover:opacity-80`,
+    tw`focus:(ring ring-white/50)`,
+    tw`active:opacity-80`,
+  ],
+  gray: [
+    tw`text-gray-500 border border-gray-500`,
+    tw`hover:opacity-80`,
+    tw`focus:(ring ring-gray-200)`,
+    tw`active:opacity-80`,
+  ],
+  yellow: [
+    tw`text-yellow-500 border border-yellow-500`,
+    tw`hover:opacity-80`,
+    tw`focus:(ring ring-yellow-200)`,
+    tw`active:opacity-80`,
+  ],
+  green: [
+    tw`text-green-500 border border-green-500`,
+    tw`hover:opacity-80`,
+    tw`focus:(ring ring-green-200)`,
+    tw`active:opacity-80`,
+  ],
+  blue: [
+    tw`text-blue-500 border border-blue-500`,
+    tw`hover:opacity-80`,
+    tw`focus:(ring ring-blue-200)`,
+    tw`active:opacity-80`,
+  ],
+  red: [
+    tw`text-red-500 border border-red-500`,
+    tw`hover:opacity-80`,
+    tw`focus:(ring ring-red-200)`,
+    tw`active:opacity-80`,
+  ],
+  purple: [
+    tw`text-purple-500 border border-purple-500`,
+    tw`hover:opacity-80`,
+    tw`focus:(ring ring-purple-200)`,
+    tw`active:opacity-80`,
+  ],
+};
